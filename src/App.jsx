5868014a866e14b6cb73841fa09038b2a609d14d
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-20 17:17:35 +0900

    filter ok

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,22 @@ const App = () => {
   const [ menuItem,setMenuItem ] = useState(menu)
   const [ categorys,setCategorys ] = useState(allCategorys)
   console.log(categorys);
+
+  const filterItems = (category) => {
+    if(category === 'all'){
+      setMenuItem(menu)
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category )
+    setMenuItem(newItems)
+  }
   
 
   return (
     <main>
       <section className="menu">
         <Title  text='our menu' />
-        <Categorys categorys={categorys} />
+        <Categorys categorys={categorys} filterItems={filterItems} />
         <Menu items={menuItem} />
       </section>
     </main>
--- src/Categorys.jsx
+++ src/Categorys.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Categorys = ({categorys}) => {
+const Categorys = ({categorys,filterItems}) => {
   return (
     <div className='btn-container'>
         {categorys.map((category) => {
             return (
-                <button className='btn' type='button' key={{category}}>{category}</button>
+                <button className='btn' type='button' key={{category}} onClick={() => filterItems(category)}>
+                    {category}
+                </button>
             )
         })}
     </div>
